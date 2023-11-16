14032a4cb5c76031d3821aa291e4ad32f40e831c
apsinghap <anshpratapsingh73511@gmail.com>
2023-11-16 17:55:39 +0530

    second commit

--- Country-Rest-API/modules/countries/countryHandler.js
+++ Country-Rest-API/modules/countries/countryHandler.js
@@ -14,7 +14,7 @@ const countryDetails = async (req, res) => {
         const details = await axios.get(url);
 
         return res.json({
-            meta: { msg: "Details found", status: true },
+            meta: { msg: ".", status: false },
             data: details.data
         });
     } catch (error) {
@@ -27,14 +27,36 @@ const countryDetails = async (req, res) => {
 
 const countryList = async (req, res) => {
     try {
-        const { filter } = req.query;
+        const { filter, sort, sortKey } = req.query;
 
         const url = `https://restcountries.com/v3.1/all?fields=${filter}`;
         const details = await axios.get(url);
 
+        // Pagination
+        const page = parseInt(req.query.page) || 1;
+        const perPage = parseInt(req.query.per_page) || 10;
+        const startIdx = (page - 1) * perPage;
+        const endIdx = startIdx + perPage;
+
+        if (Number(sort) && sortKey == "name") {
+            details.data.sort((a, b) => {
+                const nameA = a.name.common.toUpperCase();
+                const nameB = b.name.common.toUpperCase();
+
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+        } else if (Number(sort) && sortKey === "population") {
+            details.data.sort((a, b) => b.population - a.population);
+        }
         return res.json({
-            meta: { msg: "Details found", status: true },
-            data: details.data
+            meta: { msg: "Details found", status: false },
+            data: details.data.slice(startIdx, endIdx)
         });
     } catch (error) {
         return res.status(500).json({
